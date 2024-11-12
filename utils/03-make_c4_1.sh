94b337f7e46a58c9e28a7e57224db3c5cb3e1941
Denis Oleynik <nedlosster@gmail.com>
2024-11-12 18:54:32 +0000

    fix

--- utils/03-make_c4_1.sh
+++ utils/03-make_c4_1.sh
@@ -10,4 +10,6 @@ sudo rm plantuml/* -rf
 
 docker run -it --rm -v $PWD:/usr/local/structurizr structurizr/cli export -workspace workspace.dsl -format plantuml/c4plantuml -output plantuml
 
+sudo chown -R $USER plantuml
+
 popd
