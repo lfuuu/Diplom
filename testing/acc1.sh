39e4630f1041763ffbff2c25b7bd0e6dc7337b35
Denis Oleynik <nedlosster@gmail.com>
2025-05-05 22:15:18 +0300

    fix

---
+++ testing/acc1.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Биллингуем тестовые звонки звонка
+
+URL=http://0.0.0.0:8022/billing/acc
+
+curl -X 'POST' \
+  ''$URL'' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "callId": 1234,
+  "srcNumber": "74963330003",
+  "dstNumber": "79261231111",
+  "setupTime": "2025-05-05T01:01:01.000Z",
+  "connectTime": "2025-05-05T01:01:02.000Z",
+  "disconnectTime": "2025-05-05T01:01:52.000Z",
+  "sessionTime": 51,
+  "disconnectCause": 16,
+  "srcRoute": "vpbx1",
+  "dstRoute": "op1"
+}'
+
+curl -X 'POST' \
+  ''$URL'' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "callId": 3321,
+  "srcNumber": "74963330003",
+  "dstNumber": "79031233344",
+  "setupTime": "2025-05-05T01:01:01.000Z",
+  "connectTime": "2025-05-05T01:01:02.000Z",
+  "disconnectTime": "2025-05-05T01:01:52.000Z",
+  "sessionTime": 51,
+  "disconnectCause": 16,
+  "srcRoute": "vpbx1",
+  "dstRoute": "op2"
+}'
+
+curl -X 'POST' \
+  ''$URL'' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "callId": 3321,
+  "srcNumber": "79241233344",
+  "dstNumber": "74951110002",
+  "setupTime": "2025-05-05T01:01:01.000Z",
+  "connectTime": "2025-05-05T01:01:02.000Z",
+  "disconnectTime": "2025-05-05T01:01:52.000Z",
+  "sessionTime": 51,
+  "disconnectCause": 16,
+  "srcRoute": "op1",
+  "dstRoute": "vpbx1"
+}'
+
+curl -X 'POST' \
+  ''$URL'' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "callId": 3321,
+  "srcNumber": "79162345666",
+  "dstNumber": "74951110002",
+  "setupTime": "2025-05-05T01:01:01.000Z",
+  "connectTime": "2025-05-05T01:01:02.000Z",
+  "disconnectTime": "2025-05-05T01:01:52.000Z",
+  "sessionTime": 51,
+  "disconnectCause": 16,
+  "srcRoute": "op3",
+  "dstRoute": "vpbx1"
+}'
