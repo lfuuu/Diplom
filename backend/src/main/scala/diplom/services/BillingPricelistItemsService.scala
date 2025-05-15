64216843ab2d8258d750c2765a01c1da4352bebf
Denis Oleynik <nedlosster@gmail.com>
2025-05-15 21:04:10 +0300

    Двигаемся.

--- backend/src/main/scala/diplom/services/BillingPricelistItemsService.scala
+++ backend/src/main/scala/diplom/services/BillingPricelistItemsService.scala
@@ -6,12 +6,15 @@ import skunk._
 import skunk.codec.all._
 import skunk.implicits._
 import _root_.com.mcn.diplom.domain.nispd.BillingPricelistItem._
+import java.time.Instant
+import java.time.ZoneOffset
 
 trait BillingPricelistItemsService[F[_]] {
   def create(item: BillingPricelistItemCreateRequest): F[Option[BillingPricelistItemId]]
   def findAll: F[List[BillingPricelistItem]]
   def findById(id: BillingPricelistItemId): F[Option[BillingPricelistItem]]
   def deleteById(id: BillingPricelistItemId): F[Unit]
+  def matchPrefix(tm: Instant, num: String, id: BillingPricelistId): F[Option[BillingPrice]]
 }
 
 object BillingPricelistItemsService {
@@ -39,7 +42,18 @@ object BillingPricelistItemsService {
         postgres.use { session =>
           session.execute(deleteByIdPricelistItem)(id).void
         }
+
+      override def matchPrefix(tm: Instant, num: String, id: BillingPricelistId): F[Option[BillingPrice]] = {
+        val localDateUTC = tm.atZone(ZoneOffset.UTC).toLocalDate()
+        postgres.use { session =>
+          session
+            .prepare(matchPrefixPricelistItem)
+            .flatMap(_.option((num, id, BillingPriceDateFrom(localDateUTC), BillingPriceDateTo(localDateUTC))))
+        }
+      }
+
     }
+
 }
 
 private object BillingPricelistItemsSQL {
@@ -49,10 +63,29 @@ private object BillingPricelistItemsSQL {
   val ndef: Codec[BillingPriceNdefId]           = int8.imap(BillingPriceNdefId(_))(_.value)
   val dateFrom: Codec[BillingPriceDateFrom]     = date.imap(BillingPriceDateFrom(_))(_.value)
   val dateTo: Codec[Option[BillingPriceDateTo]] = date.opt.imap(_.map(BillingPriceDateTo(_)))(_.map(_.value))
-  val price: Codec[BillingPrice]                = numeric(8, 4).imap(BillingPrice(_))(_.value)
+
+  val dateToWo: Codec[BillingPriceDateTo] = date.imap(BillingPriceDateTo(_))(_.value)
+
+  val price: Codec[BillingPrice] = numeric(8, 4).imap(BillingPrice(_))(_.value)
 
   val findAllCodec       = id *: pricelistId *: ndef *: dateFrom *: dateTo *: price
   val createRequestCodec = pricelistId *: ndef *: dateFrom *: dateTo *: price
+
+  val matchPrefixPricelistItem: Query[String *: BillingPricelistId *: BillingPriceDateFrom *: BillingPriceDateTo *: EmptyTuple, BillingPrice] =
+    sql"""
+        WITH matches AS (
+          SELECT 
+              price,
+              length(ndef::text) as prefix_len
+          FROM billing.pricelist_item
+          WHERE left($text, length(ndef::text)) = ndef::text and pricelist_id = $pricelistId
+          and date_from <= $dateFrom and ( date_to >= $dateToWo or date_to is Null)
+      )
+      SELECT price
+      FROM matches
+      ORDER BY prefix_len DESC
+      LIMIT 1;
+     """.query(price)
 
   val selectAll: Query[Void, BillingPricelistItem] =
     sql"""
@@ -85,4 +118,5 @@ private object BillingPricelistItemsSQL {
       DELETE FROM billing.pricelist_item
       WHERE id = $id
     """.command
+
 }
